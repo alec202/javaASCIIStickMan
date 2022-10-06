de9671c22d45737817be5995f382edc39a45bce0
alec202 <alecmirambeau@gmail.com>
2022-10-06 18:32:46 -0400

    Create ASCIIPicture.java

---
+++ ASCIIPicture.java
@@ -0,0 +1,17 @@
+public class ASCIIPicture{
+    public static void main(String args[]){
+        System.out.println("   _______");
+        System.out.println("   | 0   0  |");
+        System.out.println("   |        |\n   | |----| |");
+        System.out.println("    _______");
+        System.out.println("      |");
+        System.out.println("     /|\\");
+        System.out.println("    / | \\");
+        System.out.println("   /  |  \\");
+        System.out.println("      |  ");
+        System.out.println("      |  ");
+        System.out.println("     / \\");
+        System.out.println("    /   \\");
+        System.out.println("   /     \\");
+    }
+}
